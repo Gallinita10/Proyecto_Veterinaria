ce796ffadd268133ec4ed93023ec731ab87c5b1c
Enzo <enzo-adrian2100@outlook.com>
2022-09-12 14:45:14 +0200

    Enzo 12/9/2022

---
+++ src/main/java/Control/Conexion.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Control;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Enzo
+ */
+public class Conexion {
+    public static Connection getConexion(){
+        
+        return null;
+        
+    }
+}
